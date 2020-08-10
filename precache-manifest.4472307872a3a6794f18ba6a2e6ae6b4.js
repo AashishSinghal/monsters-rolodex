698ffff6eb78563e52d3ebfa8548bb802d41744e
BarbarianO_o <aashish451singhal@gmail.com>
2020-08-11 02:40:42 +0530

    Updates

---
+++ precache-manifest.4472307872a3a6794f18ba6a2e6ae6b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d4122f152d01470cbcf5407792f037",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "27a845daf502fda4417b",
+    "url": "/monsters-rolodex/static/css/main.04c298f8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a845daf502fda4417b",
+    "url": "/monsters-rolodex/static/js/main.599e33ab.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
